9309ad3f7a984883ec09649588b1490ecc2c68d8
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-21 19:59:11 +0530

    [Sachin] Adding Line of Length

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -11,7 +11,20 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
+            Console.WriteLine("Enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Line length is " + length);
+
+
+
             Console.ReadLine(); 
+
         }
     }
 }
